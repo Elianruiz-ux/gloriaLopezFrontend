f000bf3ccedc4b8f864d9be72b27dd9e856212fa
El Brayan <57419379+brayan2003@users.noreply.github.com>
2022-12-04 16:36:39 -0500

    backend actualizado

---
+++ Backend/routes/authentication.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('../lib/passport');
+/*
+router.get('/signup', (req,res)=>{
+
+    res.render('')
+});
+router.post('signup',(req,res)=>{
+    passport.authenticate('local.sigup')
+    res.send('received');
+   
+});
+router.post('/signup', )*/
+module.exports = router;
---
+++ Backend/views/index.js
@@ -0,0 +1,46 @@
+//En este archivo arranca la aplicación
+
+const express = require('express');
+const morgan = require('morgan');
+const passport =require ('passport'); 
+
+//Initializations
+
+const app = express();
+const port = (process.env.port || 3000);
+require("../lib/passport");
+
+//Settings
+
+app.set('port', port);
+
+//Middlewares (funciones que se ejecutan cada vez que un usuario manda una petición)
+
+app.use(morgan('dev')); //Utiliza el modulo morgan y lo ejecuta con la palara 'dev'
+app.use(express.json());
+//app.use(passport.initialize());
+//app.use(passport.session());
+
+
+
+
+//Global variables
+
+
+//Routes
+
+app.use(require('../routes/index'));
+app.use(require('../routes/authentication'));
+app.use('/api', require('../routes/links'));
+
+//Public (todo el codigo al que el navegación puede acceder)
+
+
+//Starting the server
+
+app.listen(app.get('port'), (err) => {
+    if (err) {
+    console.log('Error al iniciar el servidor: ' + err)
+    }
+    console.log('Server inciando en el puerto: ' + app.get('port'));
+});
