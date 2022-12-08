6cef6f6e15b9b0716d94a37870784c87953c11c3
El Brayan <57419379+brayan2003@users.noreply.github.com>
2022-12-07 20:07:45 -0500

    backend terminado

--- Backend/routes/empleado.js
+++ Backend/routes/empleado.js
@@ -14,4 +14,132 @@ router.get('/', (req, res) => {
     })
 });
 
+//Un empleado
+
+router.get('/:IDENTIFICACION_EMPLEADO', (req, res) => {
+    const { IDENTIFICACION_EMPLEADO } = req.params
+    let sql = 'SELECT * FROM EMPLEADO WHERE IDENTIFICACION_EMPLEADO = ?'
+    pool.query(sql, [IDENTIFICACION_EMPLEADO], (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
+})
+
+//Post
+router.post('/', (req, res) => {
+    const { IDENTIFICACION_EMPLEADO, 
+        TIPO_DOCUMENTO, 
+        NOMBRE_EMPLEADO, 
+        FECHA_NACIMIENTO_EMPLEADO, 
+        DIRECCION,
+        SEXO_EMPLEADO,
+        TELEFONO,
+        CORREO,
+        EPS,
+        FONDO_PENSION,
+        ESTADO_CIVIL,
+        HIJOS,
+        ESTUDIOS,
+        TIPO_SERVICIO
+     } = req.body
+    let sql = `INSERT INTO EMPLEADO(
+        IDENTIFICACION_EMPLEADO, 
+        TIPO_DOCUMENTO, 
+        NOMBRE_EMPLEADO, 
+        FECHA_NACIMIENTO_EMPLEADO, 
+        DIRECCION,
+        SEXO_EMPLEADO,
+        TELEFONO,
+        CORREO,
+        EPS,
+        FONDO_PENSION,
+        ESTADO_CIVIL,
+        HIJOS,
+        ESTUDIOS,
+        TIPO_SERVICIO) 
+    values (
+    '${IDENTIFICACION_EMPLEADO}',
+    '${TIPO_DOCUMENTO}',
+    '${NOMBRE_EMPLEADO}',
+    '${FECHA_NACIMIENTO_EMPLEADO}',
+    '${DIRECCION}',
+    '${SEXO_EMPLEADO}',
+    '${TELEFONO}',
+    '${CORREO}',
+    '${EPS}',
+    '${FONDO_PENSION}',
+    '${ESTADO_CIVIL}',
+    '${HIJOS}',
+    '${ESTUDIOS}',
+    '${TIPO_SERVICIO}'
+    )`
+    pool.query(sql, (err, rows, fields) => {
+        if (err) throw err
+        else {
+            res.json({ status: 'empleado agregado' })
+        }
+    })
+});
+
+//Delete
+router.delete('/:IDENTIFICACION_EMPLEADO', (req, res) => {
+    const { IDENTIFICACION_EMPLEADO } = req.params
+
+    let sql = `DELETE FROM EMPLEADO WHERE IDENTIFICACION_EMPLEADO = '${IDENTIFICACION_EMPLEADO}'`
+    pool.query(sql, (err, rows, fields) => {
+        if (err) throw err
+        else {
+            res.json({ status: 'Se elimino el proveedor' })
+        }
+    })
+}); 
+
+
+//Put
+router.put('/:ID', (req, res) => {
+    const {ID} = req.params
+
+    const {
+        IDENTIFICACION_EMPLEADO, 
+        TIPO_DOCUMENTO, 
+        NOMBRE_EMPLEADO, 
+        FECHA_NACIMIENTO_EMPLEADO, 
+        DIRECCION,
+        SEXO_EMPLEADO,
+        TELEFONO,
+        CORREO,
+        EPS,
+        FONDO_PENSION,
+        ESTADO_CIVIL,
+        HIJOS,
+        ESTUDIOS,
+        TIPO_SERVICIO
+    } = req.body
+
+    let sql = `update EMPLEADO set
+    IDENTIFICACION_EMPLEADO = '${IDENTIFICACION_EMPLEADO}',
+    TIPO_DOCUMENTO = '${TIPO_DOCUMENTO}',
+    NOMBRE_EMPLEADO = '${NOMBRE_EMPLEADO}',
+    FECHA_NACIMIENTO_EMPLEADO = '${FECHA_NACIMIENTO_EMPLEADO}',
+    DIRECCION = '${DIRECCION}',
+    SEXO_EMPLEADO = '${SEXO_EMPLEADO}',
+    TELEFONO = '${TELEFONO}',
+    CORREO = '${CORREO}',
+    EPS = '${EPS}',
+    FONDO_PENSION = '${FONDO_PENSION}',
+    ESTADO_CIVIL = '${ESTADO_CIVIL}',
+    HIJOS = '${HIJOS}',
+    ESTUDIOS = '${ESTUDIOS}',
+    TIPO_SERVICIO = '${TIPO_SERVICIO}'
+    where IDENTIFICACION_EMPLEADO = '${ID}'`
+
+    pool.query(sql, (err, rows, fields) => {
+        if (err) throw err
+        else {
+            res.json({status: 'Se modifico la informacion del empleado'})
+        }
+    })
+})
 module.exports = router;
